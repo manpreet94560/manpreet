8b295db7da565b8bc4fc42bd2fa4a2d182fdde59
manpreet94560 <manpreet94560@gmail.com>
2025-02-11 14:57:51 +0530

    first commidt

--- src/app/componennts/Experties.jsx
+++ src/app/componennts/Experties.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Image from 'next/image';
-import { Tilt } from 'react-tilt';
+// import { Tilt } from 'react-tilt';
 
 export default function Experties() {
   return (
@@ -11,7 +11,7 @@ export default function Experties() {
 
         <div className='row py-3'>
           <div className="col-md-4" data-aos="fade-up">
-            <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+            {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
               <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
                 <img src="/manage.jpeg" width={"100%"} alt="loading.." />
                 <div className="card-body">
@@ -19,11 +19,11 @@ export default function Experties() {
                   <p className="card-text">Skilled in organizing, leading teams, and delivering successful project outcomes.</p>
                 </div>
               </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
 
           <div className="col-md-4" data-aos="fade-up">
-          <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+          {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
             <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
               <img src="/frontend.webp" alt="loading.." />
               <div className="card-body">
@@ -31,11 +31,11 @@ export default function Experties() {
                 <p className="card-text">Experienced in HTML, CSS, JavaScript, Bootstrap, and responsive web design.</p>
               </div>
             </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
 
           <div className="col-md-4" data-aos="fade-up">
-          <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+          {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
             <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
               <img src="/backend.webp" alt="loading.." />
               <div className="card-body">
@@ -43,11 +43,11 @@ export default function Experties() {
                 <p className="card-text">Proficient in Node.js, Express, MongoDB, building robust server-side applications.</p>
               </div>
             </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
 
           <div className="col-md-4" data-aos="fade-up">
-          <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+          {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
             <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
               <img src="/website.webp" alt="loading.." />
               <div className="card-body">
@@ -55,11 +55,11 @@ export default function Experties() {
                 <p className="card-text">Develop dynamic, user-friendly websites with modern technologies and frameworks.</p>
               </div>
             </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
 
           <div className="col-md-4" data-aos="fade-up">
-          <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+          {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
             <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
               <img src="/digitalmarketing.webp" alt="loading.." />
               <div className="card-body">
@@ -67,11 +67,11 @@ export default function Experties() {
                 <p className="card-text">Expert in content creation, social media, and brand promotion.</p>
               </div>
             </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
 
           <div className="col-md-4" data-aos="fade-up">
-          <Tilt options={{ max: 25, scale:1.0, speed: 300}}>
+          {/* <Tilt options={{ max: 25, scale:1.0, speed: 300}}> */}
             <div className='card p-0 mx-3 rounded-0 bg-black text-secondary border mb-3'>
               <img src="/SEO.webp" alt="loading.." />
               <div className="card-body">
@@ -79,7 +79,7 @@ export default function Experties() {
                 <p className="card-text">Optimizing websites for higher rankings through keywords research and content strategies. </p>
               </div>
             </div>
-            </Tilt>
+            {/* </Tilt> */}
           </div>
         </div>
       </div>
